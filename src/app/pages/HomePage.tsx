0bedcafda34930384976eccf5423846406606c35
Sakshitha Uppala <sakshithauppala@gmail.com>
2026-03-01 12:55:57 +0530

    Update HomePage.tsx

--- src/app/pages/HomePage.tsx
+++ src/app/pages/HomePage.tsx
@@ -1,15 +1,24 @@
 import { Navbar } from '../components/Navbar';
 import HeroMosaic from '../components/HeroMosaic';
 import { AboutSynergia } from '../components/AboutSynergia';
+import { Schedule } from '../components/Schedule';
 import { Footer } from '../components/Footer';
 
 export function HomePage() {
   return (
     <div className="size-full min-h-screen bg-[#0a0015] overflow-x-hidden">
+
       <Navbar />
+
       <HeroMosaic />
+
       <AboutSynergia />
+
+      {/* ✅ NEW SCHEDULE SECTION */}
+      <Schedule />
+
       <Footer />
+
     </div>
   );
 }
